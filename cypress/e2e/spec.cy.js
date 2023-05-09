c21c70b5514e2d4901e6d54288337529e170ae0d
zoniak <47579292+zoniak@users.noreply.github.com>
2023-05-09 19:40:16 +0200

    testeando la web de carlos

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,8 +2,7 @@
 
 describe('My First Test', () => {
   it('clicks the link "a"', () => {
-    cy.visit('https://salvarobles.github.io/')
-
+    cy.visit('https://carlosgozalbes.github.io/')
     cy.get('a').eq(1).click()
   })
 })
